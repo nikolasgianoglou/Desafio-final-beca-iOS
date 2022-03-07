8461633ea2d767823491eeb1f9220e2e98a1dadb
nikolasgianoglou <nikolasgcoelho@gmail.com>
2022-03-07 09:43:05 -0300

    Alterando AppDelegate para iniciar viewController

--- MasterCoin/AppDelegate.swift
+++ MasterCoin/AppDelegate.swift
@@ -10,10 +10,14 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+  var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    let rootViewController = ViewController()
+    let navigationController = UINavigationController(rootViewController: rootViewController)
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    self.window?.rootViewController = navigationController
+    self.window?.makeKeyAndVisible()
     return true
   }
 
