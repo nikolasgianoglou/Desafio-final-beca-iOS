b5f58793184733c3bdd00905ee66e399bbaeadd0
nikolasgianoglou <nikolasgcoelho@gmail.com>
2022-03-17 14:48:39 -0300

    Arrumando resposta da API

--- MasterCoin/Source/Infrastructure/Constants/ApiError.swift
+++ MasterCoin/Source/Infrastructure/Constants/ApiError.swift
@@ -8,17 +8,31 @@
 import Foundation
 
 public enum ApiErrorCode: Int {
-    case serverErrorStatusCode = 500
+    case badRequest = 400
+    case unauthorized = 401
+    case forbidden = 403
+    case tooManyRequests = 429
+    case NoData = 550
 }
 
 public enum KnownApiFailures: Error {
-    case serverErrorStatusCode(Error)
+  
+  case thereIsSomethingWrongWithYourRequest(Error)
+  case youAPIKeyIsWrong(Error)
+  case yourAPIKeyDoesntHaveEnouPrivilegesToAccessThisResource(Error)
+  case youHaveExceededYouAPIKEYRateLImits(Error)
+  case youRequestedSpecificSingleItemWeDontHaveAtThis(Error)
 }
 
 ///Associated Values
 public enum ApiError: Error {
     case failure (Error, Int)
     case invalidResponse(Int)
+    case badRequest(Int)
+    case unauthorized(Int)
+    case forbidden(Int)
+    case tooManyRequests(Int)
+    case NoData(Int)
     case emptyResponse
     case unknownError(Int)
 }
